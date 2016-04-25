199ebc74e30160287256afe20d3888cf4163c6fa
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-04-25 22:03:41 +0300

    Added errored file output

--- src/Module.php
+++ src/Module.php
@@ -4,6 +4,7 @@ namespace samsonphp\less;
 use samson\resourcer\ResourceRouter;
 use samsonframework\core\LoadableInterface;
 use samsonphp\event\Event;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 /**
  * SamsonPHP LESS compiler module.
@@ -13,10 +14,15 @@ use samsonphp\event\Event;
  */
 class Module implements LoadableInterface
 {
+    /** @var \lessc LESS compiler */
+    protected $less;
+
     /** SamsonFramework load preparation stage handler */
     public function prepare()
     {
         Event::subscribe(ResourceRouter::EVENT_CREATED, array($this, 'renderer'));
+
+        $this->less = new \lessc;
 
         return true;
     }
@@ -24,17 +30,24 @@ class Module implements LoadableInterface
     /**
      * New resource file update handler.
      *
-     * @param string $type Resource type(extension)
+     * @param string $type    Resource type(extension)
      * @param string $content Resource content
+     * @param string $file    LESS file path
+     *
+     * @throws \Exception
      */
-    public function renderer($type, &$content)
+    public function renderer($type, &$content, $file)
     {
         // If CSS resource has been updated
         if ($type === 'css') {
-            $less = new \lessc;
-
-            // Read updated CSS resource file and compile it
-            $content = $less->compile($content);
+            try {
+                // Read updated CSS resource file and compile it
+                $content = $this->less->compile($content);
+            } catch (\Exception $e) {
+                $errorFile = 'cache/error_resourcer'.microtime(true).'.less';
+                file_put_contents($errorFile, $content);
+                throw new \Exception('Failed compiling LESS['.$errorFile.']:'."\n".$e->getMessage());
+            }
         }
     }
 }
